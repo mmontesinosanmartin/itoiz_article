adf7ca602597290258ea0045e7a02eaaf45b8f6f
mmontesinosanmartin <montesino.upna@gmail.com>
2020-06-02 19:04:46 +0200

    water volume calculations updated

--- R/6_volume_test.R
+++ R/6_volume_test.R
@@ -20,18 +20,18 @@ library(lakemorpho)
 # load results
 load(url("https://raw.githubusercontent.com/mmontesinosanmartin/itoiz_article/master/Data/results_analysis.RData"))
 
-alturasUTM<-projectRaster(altimetry.itoiz,crs=CRS("+init=epsg:25830")) 
+altimetryUTM<-projectRaster(altimetry.itoiz,crs=CRS("+init=epsg:25830")) 
   
 
 sel<-c(1,8,15,17,19,23,26) ## Landstat images
-sec<-(1:28)[-sel]
-names(imgs.ndwi)[sec] ## Sentinale images
+sen<-(1:28)[-sel]
+names(imgs.ndwi)[sen] ## Sentinel images
   
-b2<-array(0,length(sec)) ## Water Volume ##
-b5<-array(0,length(sec)) ## length shoreline ##
-b6<-array(0,length(sec)) ## Water surface ##
+b2<-array(0,length(sen)) ## Water Volume ##
+b5<-array(0,length(sen)) ## length shoreline ##
+b6<-array(0,length(sen)) ## Water surface ##
 
-a0<-lapply(shorelns,function(r){as(r,"Spatial")})
+a0<-lapply(shorelns[sen],function(r){as(r,"Spatial")})
 a1<-lapply(a0,function(r){spTransform(r,CRS("+init=epsg:25830"))})
 a1.coor<-lapply(a1,function(r){coordinates(r)})
 
@@ -39,30 +39,29 @@ a1.coor<-lapply(a1,function(r){coordinates(r)})
 # ANALYSIS
 ###############################################################################
 
-for (i in 1:length(sec)) { 
-a2<-a1[[i]]
-plot(a2,main=paste(i))
- 
-a2.coor<-coordinates(a2) 
-a2.poli<-Polygon(a2.coor[[1]][[1]])
+for (i in 1:length(sen)) { 
+  a2<-a1[[i]]
+  plot(a2,main=paste(i))
   
-#a3.poli<-lapply(a1.coor,function(r) Polygon(r[[1]][[1]]))
-
-Srs1 = Polygons(list(a2.poli), "s1")
-SpP = SpatialPolygons(list(Srs1)) 
-crs(SpP)<-proj4string(a2)
-centroids <- coordinates(SpP)
-x <- centroids[,1]
-y <- centroids[,2] 
-SpP.SPDF <- SpatialPolygonsDataFrame(SpP,data=data.frame(x=x, y=y,row.names=row.names(SpP)))
-                                                                                                              
-input<-lakeSurroundTopo(SpP.SPDF,alturasUTM)
-b2[i]<-lakeVolume(input)/1e+6
-b5[i]<-lakeShorelineLength(input)/1e3
-b6[i]<-lakeSurfaceArea(input)/1e4
+  a2.coor<-coordinates(a2) 
+  a2.poli<-Polygon(a2.coor[[1]][[1]])
   
- print(c("num","Volume hm3","length km","surface ha"))
- print(c(i,b2[i],b5[i],b6[i]))
+  Srs1 = Polygons(list(a2.poli), "s1")
+  SpP = SpatialPolygons(list(Srs1)) 
+  crs(SpP)<-proj4string(a2)
+  centroids <- coordinates(SpP)
+  x <- centroids[,1]
+  y <- centroids[,2] 
+  SpP.SPDF <- SpatialPolygonsDataFrame(SpP,data=data.frame(x=x, y=y,row.names=row.names(SpP)))
+  
+  input<-lakeSurroundTopo(SpP.SPDF,altimetryUTM)
+  b2[i]<-lakeVolume(input)/1e+6
+  b5[i]<-lakeShorelineLength(input)/1e3
+  b6[i]<-lakeSurfaceArea(input)/1e4
+  
+  aux<-c(i,b2[i],b5[i],b6[i])
+  names(aux)<-c("num","Volume hm3","length km","surface ha")
+  print(aux)
 }
  
 
